ae0631204d190ac1746be5721fe9173514104b40
jerryyao <jerryyao@aslcn.com.cn>
2023-02-12 14:20:21 +0800

    fileupload test

---
+++ Common/PdfHelper.cs
@@ -0,0 +1,43 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class PdfHelper
+    {
+        public void CreatePdf() {
+            using (Document document = new Document(PageSize.A4)) { 
+
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(@"D:\" + Guid.NewGuid().ToString("N") + ".pdf", FileMode.Create));
+                //先打开文档，往里写一段内容，最后关闭文档
+                document.Open();
+                //document.Add(new iTextSharp.text.Paragraph("Hello World! Hello People! Hello Sky! Hello Sun! Hello Moon! Hello Stars!"));
+                BaseFont bfTimes = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+                Font times = new Font(bfTimes, 12, iTextSharp.text.Font.ITALIC, iTextSharp.text.BaseColor.RED);
+                document.Add(new iTextSharp.text.Paragraph("Hello World!", times));
+                document.Add(new iTextSharp.text.Paragraph("Hello World2!", times));
+                document.AddTitle("这里是标题");
+                document.AddSubject("主题");
+                document.AddKeywords("关键字");
+                document.AddCreator("创建者");
+                document.AddAuthor("作者");
+                document.Close();
+                writer.Close();
+            }
+        }
+        public string ReadPdf() {
+            string pdfPath = "C:\\Users\\25142\\Desktop\\Application_form_A22119-1-9.pdf";
+            PdfReader reader = new PdfReader(pdfPath);
+            StringWriter output = new StringWriter();
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new SimpleTextExtractionStrategy()));
+            return output.ToString(); 
+        }
+    }
+}
---
+++ WebApplication1/Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebApplication1.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Test()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Upload([FromForm] IFormFileCollection files)
+        { 
+            List < FileModel > list = new List<FileModel>();
+            foreach (var file in files)
+            {
+                list.Add(new FileModel { 
+                    Name= file.Name,
+                });
+            }
+            return Json(new { files = list });
+        }
+        public IActionResult MagicNumber()
+        {
+            return View();
+        }
+    }
+    public class FileModel {
+        public string Name { get; set; }
+    }
+}
